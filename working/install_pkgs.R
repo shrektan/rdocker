86132096332188289a9c0389a69c247e1a4c32b4
shrektan <shrektan@126.com>
2018-04-29 22:29:35 +0800

    update working files

---
+++ working/install_pkgs.R
@@ -0,0 +1,23 @@
+install_pkgs <- function(pkgs) {
+  installed <- installed.packages()
+  pkgs <- pkgs[!pkgs %in% installed]
+  if (length(pkgs)) {
+    message("installing pkgs: ", paste0(pkgs, collapse = ", "))
+    install.packages(pkgs, repos = 'https://cran.rstudio.com/')
+  } else {
+    message("no new pkgs need to be installed.")
+  }
+}
+
+pkgs <- c(
+  'shiny', "shinyjs", "shinythemes", "shinydashboard",
+  "highcharter", "htmlwidgets",
+  "flexdashboard",
+  "RPostgreSQL", "RSQLServer", "RJDBC",
+  "openxlsx",
+  "dygraphs", "d3heatmap", "DiagrammeR", "dtplyr",
+  "leaflet", "extrafont", "mailR",
+  "pander", "PKI", "RJSONIO", "showtext", "treemap", "viridisLite"
+)
+
+install_pkgs(pkgs)
