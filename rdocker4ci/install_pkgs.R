acc56e9cb395dc0422e433a3d4133834a7213f13
shrektan <shrektan@126.com>
2018-04-29 22:29:57 +0800

    cleaner codes

--- rdocker4ci/install_pkgs.R
+++ rdocker4ci/install_pkgs.R
@@ -1,15 +1,34 @@
 install_pkgs <- function(pkgs) {
   installed <- installed.packages()
   pkgs <- pkgs[!pkgs %in% installed]
-  if (length(pkgs)) install.packages(pkgs, repos = 'https://cran.rstudio.com/')
+  if (length(pkgs)) {
+    message("installing pkgs: ", paste0(pkgs, collapse = ", "))
+    install.packages(pkgs, repos = 'https://cran.rstudio.com/')
+  } else {
+    message("no new pkgs need to be installed.")
+  }
 }
 
-pkgs <- c('assertthat', 'config', 'covr', 'DBI', 'debugme', 'drat', 'feather',
-          'forecast', 'fs', 'fst', 'glue', 'infuser', 'PerformanceAnalytics', 'pkgconfig',
-          'pool', 'progress', 'R6', 'RColorBrewer', 'RcppArmadillo', 'RSQLite',
-          'svglite', 'testthat', 'usethis', 'V8', 'whoami', 'withr', 'writexl',
-          'xtable', 'xts',
-          'ggthemes', 'ggrepel', 'ggpubr',
-          'bookdown', 'blogdown')
+pkgs <- c(
+  'assertthat',
+  'bookdown', 'blogdown',
+  'config', 'covr', 'DBI',
+  'debugme', 'drat', 'feather',
+  'forecast', 'fs', 'fst', 'glue', 'infuser',
+  'ggthemes', 'ggrepel', 'ggpubr',
+  'PerformanceAnalytics', 'pkgconfig', 'pool', 'progress',
+  'R6', 'RColorBrewer', 'RcppArmadillo', 'RSQLite',
+  'svglite', 'testthat',
+  'usethis',
+  'V8',
+  'whoami', 'withr', 'writexl',
+  'xtable', 'xts',
+  # pkgs that should have been installed
+  # pkgs belongs to tidyverse
+  'dplyr', 'forcats', 'httr', 'lubridate', 'purrr', 'readr', 'readxl', 'stringr',
+  'tibble', 'tidyverse', 'ggplot2', 'jsonlite', 'xml2', 'rlang', 'rvest',
+  'knitr', 'yaml'
+)
 
 install_pkgs(pkgs)
+
