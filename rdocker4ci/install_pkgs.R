babdcd723fd21fd23ede691e0833f16a45deec82
shrektan <shrektan@126.com>
2018-05-11 00:24:26 +0800

    install package rsvg

--- rdocker4ci/install_pkgs.R
+++ rdocker4ci/install_pkgs.R
@@ -16,7 +16,7 @@ pkgs <- c(
   'forecast', 'fs', 'fst', 'glue', 'infuser',
   'ggthemes', 'ggrepel', 'ggpubr',
   'PerformanceAnalytics', 'pkgconfig', 'pool', 'progress',
-  'R6', 'RColorBrewer', 'RcppArmadillo', 'RSQLite',
+  'R6', 'RColorBrewer', 'RcppArmadillo', 'RSQLite', "rsvg",
   'svglite', 'testthat',
   'usethis',
   'V8',
