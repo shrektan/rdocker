4ae2d11ac0e289bbbde7f3704dd494a9c9984c92
shrektan <shrektan@126.com>
2018-05-29 00:41:31 +0800

    the version that works...

--- rdocker4shinyserver/shiny-server.sh
+++ rdocker4shinyserver/shiny-server.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
+# ShinyServer: Make sure the directory for individual app logs exists
+sudo mkdir -p /var/log/shiny-server
+sudo mkdir -p /srv/shiny-server
+sudo chown -R shiny.shiny /srv/shiny-server # should have the writing access by default
+sudo chown -R shiny.shiny /var/log/shiny-server
 
+# RUN ShinyServer
 exec shiny-server 2>&1
+
+# Currently it's not able to RUN the two at the same time, 
+# because no source or fork command when executing this script, and 
+# I totally have no idea.
