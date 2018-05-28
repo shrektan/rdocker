28be60df835c51806ae946923b55643d4063c94e
Xianying Tan <shrektan@126.com>
2018-05-28 22:54:43 +0800

    Update shiny-server.sh

--- rdocker4shinyserver/shiny-server.sh
+++ rdocker4shinyserver/shiny-server.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# ShinyServer: Make sure the directory for individual app logs exists
-sudo mkdir -p /var/log/shiny-server
-sudo mkdir -p /srv/shiny-server
-sudo chown -R shiny.shiny /srv/shiny-server # should have the writing access by default
-sudo chown -R shiny.shiny /var/log/shiny-server
+# Make sure the directory for individual app logs exists
+mkdir -p /var/log/shiny-server
+chown shiny.shiny /var/log/shiny-server
 
-# RUN ShinyServer
 exec shiny-server 2>&1
