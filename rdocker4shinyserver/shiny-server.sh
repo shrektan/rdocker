64ddc06e9fe56e7b06debcbdb48792c2cd2e61bc
shrektan <shrektan@126.com>
2018-05-29 23:28:23 +0800

    add a test shiny app

--- rdocker4shinyserver/shiny-server.sh
+++ rdocker4shinyserver/shiny-server.sh
@@ -6,9 +6,12 @@ sudo mkdir -p /srv/shiny-server
 sudo chown -R shiny.shiny /srv/shiny-server # should have the writing access by default
 sudo chown -R shiny.shiny /var/log/shiny-server
 
+# a test app to debug (in case)
+sudo Rscript -e "file.copy(test.shiny.app::app_dir(), '/srv/shiny-server', recursive = TRUE)"
+
 # RUN ShinyServer
 exec shiny-server 2>&1
 
-# Currently it's not able to RUN the two at the same time, 
-# because no source or fork command when executing this script, and 
+# Currently it's not able to RUN the two at the same time,
+# because no source or fork command when executing this script, and
 # I totally have no idea.
