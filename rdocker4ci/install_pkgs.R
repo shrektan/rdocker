9dc36bef4a8d212a690524b5029f6a9b5d1ddcb6
Xianying Tan <shrektan@126.com>
2018-07-17 17:56:57 +0800

    add corrplot

--- rdocker4ci/install_pkgs.R
+++ rdocker4ci/install_pkgs.R
@@ -11,7 +11,7 @@ install_pkgs <- function(pkgs) {
 
 pkgs <- c(
   'assertthat',
-  'config', 'covr', 'DBI',
+  'config', 'covr', 'corrplot', 'DBI',
   'debugme', 'drat', 'feather',
   'forecast', 'fs', 'fst', 'glue', 'infuser',
   'ggthemes', 'ggrepel', 'ggpubr', 'huxtable',
