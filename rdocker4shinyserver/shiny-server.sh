74ba4231a7c9b8e3e73c7f55606b6fa27fb687a5
shrektan <shrektan@126.com>
2018-09-04 01:10:30 +0800

    improve the comments

--- rdocker4shinyserver/shiny-server.sh
+++ rdocker4shinyserver/shiny-server.sh
@@ -11,7 +11,3 @@ sudo Rscript -e "file.copy(test.shiny.app::app_dir(), '/srv/shiny-server', recur
 
 # RUN ShinyServer
 exec shiny-server 2>&1
-
-# Currently it's not able to RUN the two at the same time,
-# because no source or fork command when executing this script, and
-# I totally have no idea.
