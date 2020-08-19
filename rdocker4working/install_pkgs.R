ead91956e2aea6c1fc92fd0507eb25d1b6efe1b0
shrektan <shrektan@126.com>
2020-08-20 03:26:46 +0800

    be tired of handling the linux lib dependences...
    I can't test locally due to the network in CHina...

--- rdocker4working/install_pkgs.R
+++ rdocker4working/install_pkgs.R
@@ -3,7 +3,7 @@ install_pkgs <- function(pkgs, repos = 'https://cran.rstudio.com/') {
   pkgs <- pkgs[!pkgs %in% installed]
   if (length(pkgs)) {
     message('installing pkgs: ', paste0(pkgs, collapse = ', '))
-    install.packages(pkgs, repos = repos)
+    install.packages(pkgs, repos = repos, quiet = TRUE)
   } else {
     message('no new pkgs need to be installed.')
   }
@@ -33,7 +33,23 @@ if (FALSE) {
     "leaflet", "extrafont", "mailR",
     "pander", "PKI", "RJSONIO", "showtext", "treemap", "viridisLite", "V8",
     "keyring", 'future', 'promises', 'swagger', 'Rglpk', 'RcppRoll',
-    "plumber", "webutils", "sass", "gt"
+    "plumber", "webutils", "sass", "gt",
+    'assertthat', 'checkmate', 'commonmark',
+    'config', 'covr', 'corrplot', 'DBI', 'data.table',
+    'debugme', 'drat', 'feather', 'formatR',
+    'forecast', 'fs', 'fst', 'glue',
+    'ggthemes', 'ggrepel', 'ggpubr', 'huxtable',
+    'PerformanceAnalytics', 'pkgconfig', 'pool', 'progress', 'Rcpp',
+    'R6', 'RColorBrewer', 'RcppArmadillo', 'RSQLite', "rsvg", "RcppParallel", "roll",
+    'svglite', 'testthat',
+    'usethis',
+    'whoami', 'withr', 'writexl',
+    'xtable', 'xts', 'urltools',
+    # pkgs that should have been installed
+    # pkgs belongs to tidyverse
+    'dplyr', 'forcats', 'httr', 'lubridate', 'purrr', 'readr', 'readxl', 'stringr',
+    'tibble', 'tidyverse', 'ggplot2', 'jsonlite', 'xml2', 'rlang', 'rvest',
+    'knitr', 'yaml'
   )
   gen_code(pkgs)
 }
