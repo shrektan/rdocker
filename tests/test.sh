fb9c38b724dd42aebd4def5a90622e2909c2a594
shrektan <shrektan@126.com>
2020-11-27 02:33:47 +0800

    tweak tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,4 @@
-cmd="knitr::knit2pdf('/tests/ctex.Rnw', '/tests/ctex.tex', compiler = 'xelatex')"
+cmd="knitr::knit2pdf('/tests/ctex.Rnw', compiler = 'xelatex')"
 cmd="rmarkdown::render('/tests/test.Rmd', output_format = 'pdf_document')"
 cmd="rmarkdown::render('/tests/test.Rmd', output_format = 'html_document')"
 docker run --rm -it \
